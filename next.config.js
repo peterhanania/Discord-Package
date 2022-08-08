c3c77dc31283c0f4aeb9087257bada2f451df393
Peter <peter@pogy.xyz>
2022-08-08 18:03:46 -0400

    attempting to host using GH

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
+  assetPrefix: isProd ? '/your-github-repo-name/' : '',
   reactStrictMode: true,
   swcMinify: true,
   output: "standalone",
