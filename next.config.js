5d1c278db83f7308ab8a6cb22ceb641ce5f87c74
Peter <peter@pogy.xyz>
2022-08-08 18:06:31 -0400

    file cleanup

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
-  assetPrefix: isProd ? '/your-github-repo-name/' : '',
   reactStrictMode: true,
   swcMinify: true,
   output: "standalone",
