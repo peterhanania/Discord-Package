214c1a3dc55759d706428796ff021d0ffae66546
Max Ward <max.jp.ward@gmail.com>
2022-11-09 18:03:39 -0800

    🐛 fix yearly message graph being backward

--- components/Upload.tsx
+++ components/Upload.tsx
@@ -2198,7 +2198,7 @@ export default function Upload(): ReactElement {
               });
 
             years.forEach((year: any) => {
-              yearlyMessages.push(
+              yearlyMessages.unshift(
                 channels
                   .map((c) => c.messages)
                   .flat()
