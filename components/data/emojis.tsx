c008a7baa856555f3b4e191d344909180132c4ab
Max Ward <max.jp.ward@gmail.com>
2025-02-24 14:09:09 -0800

    create Card component and integrate into Emojis component

--- components/data/emojis.tsx
+++ components/data/emojis.tsx
@@ -5,8 +5,9 @@ import { useState } from "react";
 import Image from "next/image";
 import Twemoji from "react-twemoji";
 import { SnackbarNotificationType, useSnackbarNotification } from "hooks/useSnackbarNotification";
-import { toast, ToastContainer } from "react-toastify";
+import { toast } from "react-toastify";
 import Utils from "components/utils";
+import Card from "components/generic/card";
 
 type EmojiProps = {
 	data: any;
@@ -27,47 +28,10 @@ export default function Emojis({ data }: EmojiProps) {
 	);
 
 	return (
-		<div className="px-4 py-2 mb-2 lg:mb-0 bg-gray-300 dark:bg-[#2b2d31] animate__delay-1s rounded-lg row-span-3 relative group">
-			<div
-				id="blur_2"
-				className="absolute right-[10px] top-[10px] z-[999999] lg:hidden md:hidden group-hover:block"
-				onClick={() => {
-					const div = document.getElementById("blur_2_div");
-					if (div) {
-						div.classList.toggle("blur-xl");
-						div.classList.toggle("pointer-events-none");
-						div.classList.toggle("select-none");
-
-						const el: any = document.getElementById("blur_2_show");
-						if (el) el.classList.toggle("hidden");
-
-						const el2: any = document.getElementById("blur_2_hide");
-						if (el) el2.classList.toggle("hidden");
-					}
-				}}
-			>
-				<svg
-					id="blur_2_show"
-					xmlns="http://www.w3.org/2000/svg"
-					height="24"
-					width="24"
-					className="fill-black dark:fill-white cursor-pointer pointer-events-auto opacity-80 hover:opacity-100"
-				>
-					<path d="M12 16q1.875 0 3.188-1.312Q16.5 13.375 16.5 11.5q0-1.875-1.312-3.188Q13.875 7 12 7q-1.875 0-3.188 1.312Q7.5 9.625 7.5 11.5q0 1.875 1.312 3.188Q10.125 16 12 16Zm0-1.8q-1.125 0-1.912-.788Q9.3 12.625 9.3 11.5t.788-1.913Q10.875 8.8 12 8.8t1.913.787q.787.788.787 1.913t-.787 1.912q-.788.788-1.913.788Zm0 4.8q-3.65 0-6.65-2.038-3-2.037-4.35-5.462 1.35-3.425 4.35-5.463Q8.35 4 12 4q3.65 0 6.65 2.037 3 2.038 4.35 5.463-1.35 3.425-4.35 5.462Q15.65 19 12 19Zm0-7.5Zm0 5.5q2.825 0 5.188-1.488Q19.55 14.025 20.8 11.5q-1.25-2.525-3.612-4.013Q14.825 6 12 6 9.175 6 6.812 7.487 4.45 8.975 3.2 11.5q1.25 2.525 3.612 4.012Q9.175 17 12 17Z" />
-				</svg>
-				<svg
-					className="fill-black dark:fill-white cursor-pointer pointer-events-auto hidden opacity-80 hover:opacity-100"
-					id="blur_2_hide"
-					xmlns="http://www.w3.org/2000/svg"
-					height="24"
-					width="24"
-				>
-					<path d="m16.1 13.3-1.45-1.45q.225-1.175-.675-2.2-.9-1.025-2.325-.8L10.2 7.4q.425-.2.862-.3Q11.5 7 12 7q1.875 0 3.188 1.312Q16.5 9.625 16.5 11.5q0 .5-.1.938-.1.437-.3.862Zm3.2 3.15-1.45-1.4q.95-.725 1.688-1.588.737-.862 1.262-1.962-1.25-2.525-3.588-4.013Q14.875 6 12 6q-.725 0-1.425.1-.7.1-1.375.3L7.65 4.85q1.025-.425 2.1-.638Q10.825 4 12 4q3.775 0 6.725 2.087Q21.675 8.175 23 11.5q-.575 1.475-1.512 2.738Q20.55 15.5 19.3 16.45Zm.5 6.15-4.2-4.15q-.875.275-1.762.413Q12.95 19 12 19q-3.775 0-6.725-2.087Q2.325 14.825 1 11.5q.525-1.325 1.325-2.463Q3.125 7.9 4.15 7L1.4 4.2l1.4-1.4 18.4 18.4ZM5.55 8.4q-.725.65-1.325 1.425T3.2 11.5q1.25 2.525 3.587 4.012Q9.125 17 12 17q.5 0 .975-.062.475-.063.975-.138l-.9-.95q-.275.075-.525.112Q12.275 16 12 16q-1.875 0-3.188-1.312Q7.5 13.375 7.5 11.5q0-.275.037-.525.038-.25.113-.525Zm7.975 2.325ZM9.75 12.6Z" />
-				</svg>
-			</div>
-			<div id="blur_2_div_1">
+		<Card>
+			<div>
 				{" "}
-				<div id="blur_2_div">
+				<div>
 					{emojiType ? (
 						<ul className="flex items-center rounded-lg mb-1">
 							<li className="flex gap-2">
@@ -749,6 +713,6 @@ export default function Emojis({ data }: EmojiProps) {
 					</div>
 				</div>
 			</div>
-		</div>
+		</Card>
 	)
 }
